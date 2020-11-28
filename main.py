cef46818dd8efe3e3bac02df9b766ef43bd9d621
Rokibul Islam <rokibul630@gmail.com>
2020-11-29 00:17:49 +0530

    add pyperclip package and change the functionality to copy text to clipbaord

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 import numpy as np
-import pytesseract
-from pytesseract import Output
 import pandas as pd
 from PIL import ImageGrab
-from PIL import Image
 import cv2
-import keyboard
-from time import gmtime, strftime
-import os
 import pytesseract
-
+from pytesseract import Output
+import pyperclip
+import keyboard
 
 custom_config = r'-c preserve_interword_spaces=1 --oem 1 --psm 1 -l eng'
 
@@ -24,17 +20,11 @@ def main():
         print("\n\nprocessing...\n\n")
         img = ImageGrab.grabclipboard()
         cap = np.array(img)
-        file_name = strftime(
-            "%S_%M_%H__%Y_%m_%d", gmtime())
-        file_path = os.path.join(os.getcwd(), 'files', file_name + '.py')
         processed_image = process_img(cap)
-
-        # Thanks to the author of the answer:
-        # https://stackoverflow.com/questions/59582008/preserving-indentation-with-tesseract-ocr-4-x
+        # https://stackoverflow.com/a/59666326/9716729
         d = pytesseract.image_to_data(
             processed_image, config=custom_config, output_type=Output.DICT)
         df = pd.DataFrame(d)
-
         # clean up blanks
         df1 = df[(df.conf != '-1') & (df.text != ' ') & (df.text != '')]
         # sort blocks vertically
@@ -58,23 +48,19 @@ def main():
                     prev_line = ln['line_num']
                     prev_left = 0
 
-                added = 0  # num of spaces that should be added
+                added = 0
                 if ln['left']/char_w > prev_left + 1:
                     added = int((ln['left'])/char_w) - prev_left
                     text += ' ' * added
                 text += ln['text'] + ' '
                 prev_left += len(ln['text']) + added + 1
             text += '\n'
-            file = open(file_path, 'a')
-            file.write(text)
-            file.close()
-            os.system("code " + file_path)
-            print('saved to file: ', file_name + '.py' + "\n\n")
-
-    except:
+            pyperclip.copy(text)
+            print("copied to clipboard")
+    except(RuntimeError, TypeError, NameError):
         print("Encountered an error\n\n")
 
 
 if __name__ == "__main__":
-    keyboard.add_hotkey('ctrl+shift+alt+q', main)
+    keyboard.add_hotkey('ctrl+shift+g', main)
     keyboard.wait()
